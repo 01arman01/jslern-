78d04c6e3e6763b4dff8dbc83d4aadead394cbf7
Arman <papyandeveloper@gmail.com>
2022-07-01 01:05:55 +0400

    end lerning attributes

--- js_Browser/02/js/scripte1.js
+++ js_Browser/02/js/scripte1.js
@@ -1,3 +1,10 @@
 let block = document.querySelector('div[data-widget-name="menu"]')
 console.log(block.dataset.widgetName)
 // console.log()
+let listElements = document.querySelectorAll('ul>li>a')
+for(let element of listElements){
+
+   if(element.getAttribute('href').includes('//') && !element.getAttribute('href').includes('http://internal.com')){
+       element.style.color = "orange"
+   }
+}
